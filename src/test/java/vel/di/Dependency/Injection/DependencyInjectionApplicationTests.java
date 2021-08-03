a226ea6f91e54b99966dce28ef5808746dc6fa3c
Velmurugan <Velmuruganmecse@gmail.com>
2021-08-04 02:37:06 +0530

    Initial Versio

---
+++ src/test/java/vel/di/Dependency/Injection/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package vel.di.Dependency.Injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
